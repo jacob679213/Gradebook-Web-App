87951fc3da8cedd5e3c323757b1ee46ec0f4ba87
jacob679213 <47827860+jacob679213@users.noreply.github.com>
2020-12-08 14:48:57 -0500

    Added renderData function

--- client.js
+++ client.js
@@ -2,6 +2,38 @@
 const data = loadTestData()
 const $gradeTable = document.getElementById("gradeTable")
 
+renderData()
+
+/**
+ * Takes in student data and turns it into HTMP for the page to load
+ * @todo Make the student's name a button or a link or something, so clicking on it will show a more detailed view of what the student's grades are
+ */
+function renderData(){
+    data.students.forEach(student => {
+        const $studentGrade = document.createElement("tr")
+        $studentGrade.innerHTML = `<td>${student.lastName}, ${student.firstName}</td><td>${findAverageScore(student.assignments)}</td>`
+        $gradeTable.append($studentGrade)
+    })
+}
+
+/**
+ * Takes in a student's array of assignments and returns the average grade of those assignments
+ * 
+ * @param {array} assignments the array of assignments to read
+ * @returns {number} the average score out of 100 for the assignments
+ */
+function findAverageScore (assignments){
+    let possibleScore = 0;
+    let earnedScore = 0;
+    assignments.forEach(assignment => {
+        possibleScore += assignment.possibleScore
+        earnedScore += assignment.earnedScore
+    })
+
+    const average = earnedScore/possibleScore
+    return (average*100).toFixed(1)
+}
+
 function loadTestData(){
     const students = [
 
@@ -9,7 +41,7 @@ function loadTestData(){
             //key:value
             "lastName": "Doe",
             "firstName": "John",
-            "Assignments": [
+            "assignments": [
                 {
                     "name": "Homework 1",
                     "possibleScore": 10,
@@ -32,25 +64,39 @@ function loadTestData(){
                     "passed": true
                 }
             ]
+        },
+
+        {
+            "lastName": "Doe",
+            "firstName": "Jane",
+            "assignments": [
+                {
+                    "name": "Homework 1",
+                    "possibleScore": 10,
+                    "earnedScore": 7,
+                    "letterGrade": "C-",
+                    "passed": true
+                },
+                {
+                    "name": "Homework 2",
+                    "possibleScore": 15,
+                    "earnedScore": 7,
+                    "letterGrade": "F",
+                    "passed": false
+                },
+                {
+                    "name": "Homework 1",
+                    "possibleScore": 100,
+                    "earnedScore": 75,
+                    "letterGrade": "C",
+                    "passed": true
+                },
+            ]
         }
 
     ]
-}
 
-/**
- * Takes in a student's array of assignments and returns the average grade of those assignments
- * 
- * @param {array} assignments the array of assignments to read
- * @returns {number} the average score out of 100 for the assignments
- */
-function findAverageScore (assignments){
-    const possibleScore = 0;
-    const earnedScore = 0;
-    assignments.forEach(assignment => {
-        possibleScore += assignment.possibleScore
-        earnedScore += assignment.earnedScore
-    })
-
-    const average = earnedScore/possibleScore
-    return (average*100).toFixed(1)
+    return {
+        "students":  students
+    }
 }
